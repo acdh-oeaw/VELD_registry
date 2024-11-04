c625b980985bdb764e37d44d8eeeb2f475976816
SteffRhes <steffrhes@pm.me>
2024-11-04 22:15:41 +0100

    restructured again

--- src/convert_to_clscor.py
+++ src/convert_to_clscor.py
@@ -1,10 +1,14 @@
 import yaml
 
 
+IN_MAPPING_PATH = "/app/data/clscor_conversion/mapping.yaml"
 IN_VELD_MERGED_PATH = "/app/data/veld_files/merged/all_velds_merged.yaml"
 
 
 def main():
+    with open(IN_MAPPING_PATH, "r") as f_in:
+        mapping = yaml.safe_load(f_in)
+    
     with open(IN_VELD_MERGED_PATH, "r") as f_in:
         all_velds = yaml.safe_load(f_in)
         for veld_url_content in all_velds.values():
