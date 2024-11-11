3968d74ccaef8b10739c9a534278803e61f3d062
SteffRhes <steffrhes@pm.me>
2024-11-11 13:19:19 +0100

    change order of processing from mapping first, veld second, to veld first, mapping second

--- src/convert_to_clscor.py
+++ src/convert_to_clscor.py
@@ -8,85 +8,89 @@ IN_VELD_DATA_PATH = "/app/data/veld_files/merged/all_velds_merged.yaml"
 
 with open(IN_VELD_DATA_PATH, "r") as f:
     VELD_DATA_ALL = yaml.safe_load(f)
+VELD_DATA_CURRENT = None
     
     
-def _get_veld_uri_by_type(veld_type):
-    result = {}
-    for k, v in VELD_DATA_ALL.items():
-        try:
-            _ = v["content"]["x-veld"][veld_type]
-            _ = v["url"]
-        except KeyError:
-            continue
-        else:
-            result[k] = [v["url"]]
-    return result
-
-
-def get_data_veld_uris():
-    result = _get_veld_uri_by_type("data")
-    return result
-
-
-def _get_data_veld_uris__as_chain_input_or_output(direction):
-    result = {}
-    for k, v in VELD_DATA_ALL.items():
-        try:
-            _ = v["content"]["x-veld"]["chain"]
-            services = v["content"]["services"]
-            volumes_list = []
-            for s in services.values():
-                for vol in s.get("volumes"):
-                    volumes_list.append(vol)
-        except:
-            continue
-        else:
-            chain_data_uris = []
-            data_veld_uris = get_data_veld_uris()
-            for vol in volumes_list:
-                vol = vol.split(":")
-                if direction in vol[1]:
-                    chain_data_path = vol[0][2:]
-                    chain_data_path = chain_data_path.split("/")
-                    match_count_max = 0
-                    for data_id, data_uri in data_veld_uris.items():
-                        data_id = data_id.split("___")
-                        match_count_tmp = 0
-                        for i in range(min(len(chain_data_path), len(data_id))):
-                            if chain_data_path[i] == data_id[i]:
-                                match_count_tmp += 1
-                        if match_count_tmp > match_count_max:
-                            match_count_max = match_count_tmp
-                            if match_count_max > 0:
-                                chain_data_uris.extend(data_uri)
-            result[k] = chain_data_uris
-    return result
-
-
-def get_data_veld_uris__as_chain_input():
-    result = _get_data_veld_uris__as_chain_input_or_output("input")
-    return result
-
-
-def get_data_veld_uris__as_chain_output():
-    result = _get_data_veld_uris__as_chain_input_or_output("output")
-    return result
-
-
-def get_code_veld_uris():
-    result = _get_veld_uri_by_type("code")
-    return result
-
-
-def get_chain_veld_uris():
-    result = _get_veld_uri_by_type("chain")
-    return result
-
-
-def _get_topic(v):
+def _get_veld_uri_by_type(veld_data, veld_type):
     result = []
     try:
-        topics = list(v["content"]["x-veld"].values())[0]["topics"]
+        _ = veld_data["content"]["x-veld"][veld_type]
+        _ = veld_data["url"]
+    except KeyError:
+        pass
+    else:
+        result = [veld_data["url"]]
+    return result
+
+
+def get_data_veld_uris(veld_data):
+    result = _get_veld_uri_by_type(veld_data, "data")
+    return result
+
+
+def _get_data_veld_uris__as_chain_input_or_output(veld_data, direction):
+    result = []
+    try:
+        _ = veld_data["content"]["x-veld"]["chain"]
+        services = veld_data["content"]["services"]
+        volumes_list = []
+        for s in services.values():
+            for vol in s.get("volumes"):
+                volumes_list.append(vol)
+    except:
+        pass
+    else:
+        data_veld_uris = {}
+        for data_veld_id, data_veld_data in VELD_DATA_ALL.items():
+            try:
+                _ = data_veld_data["content"]["x-veld"]["data"]
+            except:
+                pass
+            else:
+                data_veld_uris[data_veld_id] = data_veld_data["url"]
+        for vol in volumes_list:
+            vol = vol.split(":")
+            if direction in vol[1]:
+                chain_data_path = vol[0][2:]
+                chain_data_path = chain_data_path.split("/")
+                match_count_max = 0
+                for data_id, data_uri in data_veld_uris.items():
+                    data_id = data_id.split("___")
+                    match_count_tmp = 0
+                    for i in range(min(len(chain_data_path), len(data_id))):
+                        if chain_data_path[i] == data_id[i]:
+                            match_count_tmp += 1
+                    if match_count_tmp > match_count_max:
+                        match_count_max = match_count_tmp
+                        if match_count_max > 0:
+                            result.append(data_uri)
+    return result
+
+
+def get_data_veld_uris__as_chain_input(veld_data):
+    result = _get_data_veld_uris__as_chain_input_or_output(veld_data, "input")
+    return result
+
+
+def get_data_veld_uris__as_chain_output(veld_data):
+    result = _get_data_veld_uris__as_chain_input_or_output(veld_data, "output")
+    return result
+
+
+def get_code_veld_uris(veld_data):
+    result = _get_veld_uri_by_type(veld_data, "code")
+    return result
+
+
+def get_chain_veld_uris(veld_data):
+    result = _get_veld_uri_by_type(veld_data, "chain")
+    return result
+
+
+def _get_topic(veld_data):
+    result = []
+    try:
+        topics = list(veld_data["content"]["x-veld"].values())[0]["topics"]
     except:
         pass
     else:
@@ -97,132 +101,112 @@ def _get_topic(v):
     return result
 
 
-def get_code_reification_to_topic():
-    result = {}
-    i = 1
-    for k, v in VELD_DATA_ALL.items():
-        try:
-            _ = v["content"]["x-veld"]["code"]
-            topics = v["content"]["x-veld"]["code"]["topics"]
-        except:
-            continue
-        else:
-            if topics != "" and topics != [] and topics is not None:
-                result[k] = [f"clscor:/code_reification_to_topic_{i}"]
-                i += 1
+def get_code_reification_to_topic(veld_data):
+    result = []
+    i = 1 # TODO: solve problem with global index
+    try:
+        _ = veld_data["content"]["x-veld"]["code"]
+        topics = veld_data["content"]["x-veld"]["code"]["topics"]
+    except:
+        pass
+    else:
+        if topics != "" and topics != [] and topics is not None:
+            result = [f"clscor:/code_reification_to_topic_{i}"]
+            i += 1
     return result
 
 
-def get_topic_of_code_reification_to_topic():
-    result = {}
-    for k, v in VELD_DATA_ALL.items():
-        try:
-            _ = v["content"]["x-veld"]["code"]
-        except:
-            continue
-        else:
-            result[k] = _get_topic(v)
+def get_topic_of_code_reification_to_topic(veld_data):
+    result = []
+    try:
+        _ = veld_data["content"]["x-veld"]["code"]
+    except:
+        pass
+    else:
+        result = _get_topic(veld_data)
     return result
 
 
-def get_code_input_file_type():
-    result = {}
-    for k, v in VELD_DATA_ALL.items():
-        try:
-            _ = v["content"]["x-veld"]["code"]
-            input_group_list = v["content"]["x-veld"]["code"]["inputs"]
-        except:
-            continue
-        else:
-            inputs = []
-            for input_group in input_group_list:
-                input_group_file_type = input_group["file_type"]
-                if type(input_group_file_type) is list:
-                    for ft in input_group_file_type:
-                        inputs.append(ft)
-                elif type(input_group_file_type) is str:
-                    inputs.append(input_group_file_type)
-            if inputs != [] and inputs is not None:
-                result[k] = inputs
+def get_code_input_file_type(veld_data):
+    result = []
+    try:
+        _ = veld_data["content"]["x-veld"]["code"]
+        input_group_list = veld_data["content"]["x-veld"]["code"]["inputs"]
+    except:
+        pass
+    else:
+        inputs = []
+        for input_group in input_group_list:
+            input_group_file_type = input_group["file_type"]
+            if type(input_group_file_type) is list:
+                for ft in input_group_file_type:
+                    inputs.append(ft)
+            elif type(input_group_file_type) is str:
+                inputs.append(input_group_file_type)
+        if inputs != [] and inputs is not None:
+            result = inputs
     return result
 
 
-def get_integrated_code_veld_id():
-    result = {}
-    code_veld_instances = get_code_veld_uris()
-    for k, v in VELD_DATA_ALL.items():
-        code_veld_url = None
-        try:
-            _ = v["content"]["x-veld"]["chain"]
-            services = v["content"]["services"]
-            code_veld_file_list = [s["extends"]["file"] for s in services.values()]
-        except:
-            continue
-        else:
-            code_veld_url_list = []
-            for code_veld_file in code_veld_file_list:
-                code_veld_id = code_veld_file[2:].replace("/", "___")
-                code_veld_url = code_veld_instances.get(code_veld_id)
-                if code_veld_url is not None:
-                    code_veld_url_list.extend(code_veld_url)
-            if code_veld_url_list != []:
-                result[k] = code_veld_url_list
+def get_integrated_code_veld_id(veld_data):
+    result = []
+    try:
+        _ = veld_data["content"]["x-veld"]["chain"]
+        services = veld_data["content"]["services"]
+        code_veld_file_list = [s["extends"]["file"] for s in services.values()]
+    except:
+        pass
+    else:
+        for code_veld_file in code_veld_file_list:
+            code_veld_id = code_veld_file[2:].replace("/", "___")
+            code_veld_data = VELD_DATA_ALL.get(code_veld_id)
+            if code_veld_data is not None:
+                code_veld_uri = get_code_veld_uris(code_veld_data)
+                if code_veld_uri is not None:
+                    result.extend(code_veld_uri)
     return result
 
 
-def get_chain_topic_as_x6():
+def get_chain_topic_as_x6(veld_data):
     result = {}
-    for k, v in VELD_DATA_ALL.items():
-        try:
-            _ = v["content"]["x-veld"]["chain"]
-        except:
-            continue
-        else:
-            result[k] = _get_topic(v)
+    try:
+        _ = veld_data["content"]["x-veld"]["chain"]
+    except:
+        pass
+    else:
+        result = _get_topic(veld_data)
     return result
 
 
-def get_code_topic_as_x6():
-    result = {}
-    for k, v in VELD_DATA_ALL.items():
-        try:
-            _ = v["content"]["x-veld"]["code"]
-        except:
-            continue
-        else:
-            result[k] = _get_topic(v)
-    return result
-
-
-def do_scopes_align(k1, k2, k3):
-    
-    def do_two_align(k1, k2):
-        result = (k1 is None or k2 is None) or (k1 == k2)
-        return result
-    
-    result = do_two_align(k1, k2) and do_two_align(k1, k3) and do_two_align(k2, k3)
+def get_code_topic_as_x6(veld_data):
+    result = []
+    try:
+        _ = veld_data["content"]["x-veld"]["code"]
+    except:
+        pass
+    else:
+        result = _get_topic(veld_data)
     return result
 
 
 def main():
     from data.clscor_conversion.mapping import mappings
     
-    for m_id, m in mappings.items():
-        print(m_id)
-        m_instantiated = {}
-        for k, v in m.items():
-            if type(v) is types.FunctionType:
-                m_instantiated[k] = v()
-            else:
-                m_instantiated[k] = {None: [v]}
-        for s_scope, s_list in m_instantiated["s"].items():
-            for p_scope, p_list in m_instantiated["p"].items():
-                for o_scope, o_list in m_instantiated["o"].items():
-                    if do_scopes_align(s_scope, p_scope, o_scope):
-                        for s in s_list:
-                            for p in p_list:
-                                for o in o_list:
-                                    print(s, p, o)
+    for veld_key, veld_data in VELD_DATA_ALL.items():
+        print("# VELD: ", veld_key)
+        for m_id, m in mappings.items():
+            print("## mapping: ", m_id)
+            m_instantiated = {}
+            for k, v in m.items():
+                if type(v) is types.FunctionType:
+                    m_instantiated[k] = v(veld_data)
+                else:
+                    m_instantiated[k] = [v]
+            for s in m_instantiated["s"]:
+                for p in m_instantiated["p"]:
+                    for o in m_instantiated["o"]:
+                        print(s, p, o)
+        print()
                                     
 
 if __name__ == "__main__":
