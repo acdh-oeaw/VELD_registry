b167205955e7f6f5f72da5a112acf7033727e408
SteffRhes <steffrhes@pm.me>
2025-02-18 23:54:39 +0100

    make E13 uri unique with relation to Y8 attribute

--- data/clscor_conversion/mapping.py
+++ data/clscor_conversion/mapping.py
@@ -87,27 +87,27 @@ mappings = {
     
     # Attribute Assignments
     "assignment_uri a E13": {
-        "s": get_attribute_assignment_uris,
+        "s": get_attribute_assignment_uris_y8,
         "p": RDF.type,
         "o": CRM["E13_Attribute_Assignment"]
     },
     "assignment_uri P134 tool_description_uri": {
-        "s": get_attribute_assignment_uris,
+        "s": get_attribute_assignment_uris_y8,
         "p": CRM["P134_continued"],
         "o": get_cls_tool_description_event_uris
     },
     "assignment_uri P140 code or chain veld url": {
-        "s": get_attribute_assignment_uris,
+        "s": get_attribute_assignment_uris_y8,
         "p": CRM["P140_assigned_attribute_to"],
         "o": get_code_or_chain_veld_yaml_url,
     },
     "assignment_uri P141 method uri": {
-        "s": get_attribute_assignment_uris,
+        "s": get_attribute_assignment_uris_y8,
         "p": CRM["P141_assigned"],
         "o": get_method_uris,
     },
     "assignment_uri P177 Y8": {
-        "s": get_attribute_assignment_uris,
+        "s": get_attribute_assignment_uris_y8,
         "p": CRM["P177_assigned_property_of_type"],
         "o": CRMCLS["Y8_implements"],
     },
